c04082ec08229d3f854dfc5085d93de6849d5da5
rudskaya irina <irarudskaya5@gmail.com>
2023-07-05 05:15:44 +0300

    Создание класса EmployeeDAOImpl

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,52 +3,52 @@ import java.sql.*;
 public class Application {
     public static void main(String[] args) {
 
-        // Задаем параметры для подключения к базе данных:
-        // имя пользователя, пароль и URL
-        final String user = "postgres";
-        final String password = "7899";
-        final String url = "jdbc:postgresql://localhost:5432/skypro";
-
-        // Создаем соединение с базой данных внутри try-with-resources,
-        // чтобы автоматически закрыть его после использования
-        try (final Connection connection =
-                     DriverManager.getConnection(url, user, password);
-             // Создаем объект PreparedStatement с SQL-запросом
-             // для выборки данных об одном из работников(имя, фамилия, пол, город) по id
-             PreparedStatement statement =
-                     connection.prepareStatement("SELECT * FROM city RIGHT JOIN employee ON city.city_id = employee.city_id WHERE id = 4")) {
-
-            // Выполняем SQL-запрос
-            // и получаем результаты в виде объекта ResultSet
-            ResultSet resultSet = statement.executeQuery();
-
-            // Обрабатываем результаты запроса, перебирая все записи в таблице
-            while (resultSet.next()) {
-                // Получаем значения полей записи и выводим их на экран
-                int idOfEmployee = resultSet.getInt("id");
-                System.out.println("ID сотрудника: " + idOfEmployee);
-
-                String firstName = resultSet.getString("first_name");
-                System.out.println("Имя сотрудника: " + firstName);
-
-                String lastName = resultSet.getString("last_name");
-                System.out.println("Фамилия сотрудника: " + lastName);
-
-                String gender = resultSet.getString("gender");
-                System.out.println("Пол: " + gender);
-
-                int age = resultSet.getInt("age");
-                System.out.println("Возраст: " + age);
-
-                String cityName = resultSet.getString("city_name");
-                System.out.println("Город: " + cityName);
-            }
-
-        } catch (SQLException e) {
-            // В случае возникновения ошибки при работе с базой данных
-            // выводим сообщение об ошибке и информацию об исключении
-            System.out.println("Ошибка при подключении к базе данных!");
-            e.printStackTrace();
-        }
+//        // Задаем параметры для подключения к базе данных:
+//        // имя пользователя, пароль и URL
+//        final String user = "postgres";
+//        final String password = "7899";
+//        final String url = "jdbc:postgresql://localhost:5432/skypro";
+//
+//        // Создаем соединение с базой данных внутри try-with-resources,
+//        // чтобы автоматически закрыть его после использования
+//        try (final Connection connection =
+//                     DriverManager.getConnection(url, user, password);
+//             // Создаем объект PreparedStatement с SQL-запросом
+//             // для выборки данных об одном из работников(имя, фамилия, пол, город) по id
+//             PreparedStatement statement =
+//                     connection.prepareStatement("SELECT * FROM city RIGHT JOIN employee ON city.city_id = employee.city_id WHERE id = 4")) {
+//
+//            // Выполняем SQL-запрос
+//            // и получаем результаты в виде объекта ResultSet
+//            ResultSet resultSet = statement.executeQuery();
+//
+//            // Обрабатываем результаты запроса, перебирая все записи в таблице
+//            while (resultSet.next()) {
+//                // Получаем значения полей записи и выводим их на экран
+//                int idOfEmployee = resultSet.getInt("id");
+//                System.out.println("ID сотрудника: " + idOfEmployee);
+//
+//                String firstName = resultSet.getString("first_name");
+//                System.out.println("Имя сотрудника: " + firstName);
+//
+//                String lastName = resultSet.getString("last_name");
+//                System.out.println("Фамилия сотрудника: " + lastName);
+//
+//                String gender = resultSet.getString("gender");
+//                System.out.println("Пол: " + gender);
+//
+//                int age = resultSet.getInt("age");
+//                System.out.println("Возраст: " + age);
+//
+//                String cityName = resultSet.getString("city_name");
+//                System.out.println("Город: " + cityName);
+//            }
+//
+//        } catch (SQLException e) {
+//            // В случае возникновения ошибки при работе с базой данных
+//            // выводим сообщение об ошибке и информацию об исключении
+//            System.out.println("Ошибка при подключении к базе данных!");
+//            e.printStackTrace();
+//        }
     }
 }
--- src/main/java/EmployeeDAO.java
+++ src/main/java/EmployeeDAO.java
@@ -1,16 +1,18 @@
-import lesson_4.Book;
-
 import java.util.List;
 
 public interface EmployeeDAO {
     // создание (добавление) сущности Employee в таблицу
-    List<Employee> addEmployee(Employee employee);
+    void addEmployee(Employee employee);
+
     // получение конкретного объекта Employee по id
-    List<Employee> findEmployeeById(int id);
+    Employee findEmployeeById(int id);
+
     // получение списка всех объектов Employee из базы
     List<Employee> getAllEmployee();
+
     // изменение конкретного объекта Employee в базе по id
-    List<Employee> setEmployeeById(int id);
+    void setEmployeeById(int id);
+
     // удаление конкретного объекта Employee из базы по id
-    List<Employee> deleteEmployeeById(int id);
+    void deleteEmployeeById(int id);
 }
