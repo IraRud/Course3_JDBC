0778bb9d78895861845d59d935ccf28024f84b94
rudskaya irina <irarudskaya5@gmail.com>
2023-07-05 03:18:32 +0300

    Получение и вывод в консоль полных данных об одном из работников(имя, фамилия, пол, город) по id

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -12,9 +12,37 @@ public class Application {
         // Создаем соединение с базой данных внутри try-with-resources,
         // чтобы автоматически закрыть его после использования
         try (final Connection connection =
-                     DriverManager.getConnection(url, user, password)) {
+                     DriverManager.getConnection(url, user, password);
+             // Создаем объект PreparedStatement с SQL-запросом
+             // для выборки данных об одном из работников(имя, фамилия, пол, город) по id
+             PreparedStatement statement =
+                     connection.prepareStatement("SELECT * FROM city RIGHT JOIN employee ON city.city_id = employee.city_id WHERE id = 4")) {
 
-            System.out.println("Соединение установлено!");
+            // Выполняем SQL-запрос
+            // и получаем результаты в виде объекта ResultSet
+            ResultSet resultSet = statement.executeQuery();
+
+            // Обрабатываем результаты запроса, перебирая все записи в таблице
+            while (resultSet.next()) {
+                // Получаем значения полей записи и выводим их на экран
+                int idOfEmployee = resultSet.getInt("id");
+                System.out.println("ID сотрудника: " + idOfEmployee);
+
+                String firstName = resultSet.getString("first_name");
+                System.out.println("Имя сотрудника: " + firstName);
+
+                String lastName = resultSet.getString("last_name");
+                System.out.println("Фамилия сотрудника: " + lastName);
+
+                String gender = resultSet.getString("gender");
+                System.out.println("Пол: " + gender);
+
+                int age = resultSet.getInt("age");
+                System.out.println("Возраст: " + age);
+
+                String cityName = resultSet.getString("city_name");
+                System.out.println("Город: " + cityName);
+            }
 
         } catch (SQLException e) {
             // В случае возникновения ошибки при работе с базой данных
