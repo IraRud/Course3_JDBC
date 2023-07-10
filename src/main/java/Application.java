bea01ffd3f141e96eec7f88f7ad1334991d50c22
rudskaya irina <irarudskaya5@gmail.com>
2023-07-11 01:52:17 +0300

    Новая реализация Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,4 @@
-import config.ConnectionConfig;
 import model.Employee;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Application {
     public static void main(String[] args) {
@@ -36,39 +31,39 @@ public class Application {
     }
 
 
-    private static void task_1() {
-
-        try (final PreparedStatement statement =
-                     ConnectionConfig.getConnection().prepareStatement("SELECT * FROM employee WHERE id < 4")) {
-
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-
-                int idOfEmployee = resultSet.getInt("id");
-                System.out.println("ID сотрудника: " + idOfEmployee);
-
-                String firstName = resultSet.getString("first_name");
-                System.out.println("Имя сотрудника: " + firstName);
-
-                String lastName = resultSet.getString("last_name");
-                System.out.println("Фамилия сотрудника: " + lastName);
-
-                String gender = resultSet.getString("gender");
-                System.out.println("Пол: " + gender);
-
-                int age = resultSet.getInt("age");
-                System.out.println("Возраст: " + age);
-
-                int cityName = resultSet.getInt("city_id");
-                System.out.println("Город: " + cityName);
-                System.out.println();
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Ошибка при подключении к базе данных!");
-            e.printStackTrace();
-        }
-    }
+//    private static void task_1() {
+//
+//        try (final PreparedStatement statement =
+//                     ConnectionConfig.getConnection().prepareStatement("SELECT * FROM employee WHERE id < 4")) {
+//
+//            ResultSet resultSet = statement.executeQuery();
+//
+//            while (resultSet.next()) {
+//
+//                int idOfEmployee = resultSet.getInt("id");
+//                System.out.println("ID сотрудника: " + idOfEmployee);
+//
+//                String firstName = resultSet.getString("first_name");
+//                System.out.println("Имя сотрудника: " + firstName);
+//
+//                String lastName = resultSet.getString("last_name");
+//                System.out.println("Фамилия сотрудника: " + lastName);
+//
+//                String gender = resultSet.getString("gender");
+//                System.out.println("Пол: " + gender);
+//
+//                int age = resultSet.getInt("age");
+//                System.out.println("Возраст: " + age);
+//
+//                int cityName = resultSet.getInt("city_id");
+//                System.out.println("Город: " + cityName);
+//                System.out.println();
+//            }
+//
+//        } catch (SQLException e) {
+//            System.out.println("Ошибка при подключении к базе данных!");
+//            e.printStackTrace();
+//        }
+//    }
 
 }
