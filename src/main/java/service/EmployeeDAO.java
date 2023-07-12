87b2157ab33e46014f13740fbc6a0fd8e90561bd
rudskaya irina <irarudskaya5@gmail.com>
2023-07-13 00:26:44 +0300

    CityDAOImpl

--- src/main/java/service/EmployeeDAO.java
+++ src/main/java/service/EmployeeDAO.java
@@ -5,18 +5,14 @@ import model.Employee;
 import java.util.List;
 
 public interface EmployeeDAO {
-    // создание (добавление) сущности Employee в таблицу
+
     void addEmployee(Employee employee);
 
-    // получение конкретного объекта Employee по id
     Employee findEmployeeById(int id);
 
-    // получение списка всех объектов Employee из базы
     List<Employee> getAllEmployees();
 
-    // изменение конкретного объекта Employee в базе
     void setEmployee(Employee employee);
 
-    // удаление конкретного объекта Employee из базы
     void deleteEmployee(Employee employee);
 }
